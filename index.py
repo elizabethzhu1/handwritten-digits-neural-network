31e15f0a0a048e5d0d95212f57ef88a6373ec652
Elizabeth Zhu <elizabethzhuca@gmail.com>
2023-08-19 21:52:24 -0700

    small edits

--- index.py
+++ index.py
@@ -14,11 +14,16 @@ mnist = tf.keras.datasets.mnist
 x_train = tf.keras.utils.normalize(x_train, axis=1)
 x_test = tf.keras.utils.normalize(x_test, axis=1)
 
+# x_train = x_train / 255
+# x_test = x_test / 255
+
 # create model with several layers
 model = tf.keras.models.Sequential()
 model.add(tf.keras.layers.Flatten(input_shape=(28, 28))) # 28 x 28 pixels
 model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu)) # rectified linear unit (relu)
+# model.add(tf.keras.layers.Dropout(0.5))  # Drop 50% of the nodes
 model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+# model.add(tf.keras.layers.Dropout(0.5))  # Drop 50% of the nodes
 model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax)) # output layer - each unit represents a digit
 # note: softmax is a probability distribution function, with all digit probabilities adding up to 1
 
@@ -44,6 +49,7 @@ image_number = 1
 while os.path.isfile(f"digits/{image_number}.png"):
     img = cv2.imread(f"digits/{image_number}.png")[:,:,0] # read image as grayscale
     img = np.invert(np.array([img])) # invert image to match training data
+    # img = tf.keras.utils.normalize(img, axis=1) # normalize image
     prediction = model.predict(img)
     print(f"The number is probably a {np.argmax(prediction)}!") # argmax gives neuron with highest activation
     plt.imshow(img[0], cmap=plt.cm.binary) # show image
